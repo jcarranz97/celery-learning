18804bef0dab31c13703a4b9ef0577e7ea6def00
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-02-25 02:45:24 -0600

    Start implementing oauth2

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import asyncio
 from fastapi import FastAPI
 from fastapi import WebSocket
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
 from celery.result import AsyncResult
+from fastapi import status
 from celery_worker import celery_app
 from group1.router import router as group1_router
 from group2.router import router as group2_router
+from auth import fake_users_db
+from auth import UserInDB
+from auth import get_current_active_user
+from auth import User
+from typing import Annotated
+from auth import authenticate_user
+from auth import Token
+from auth import create_access_token
 
 
 app = FastAPI()
@@ -33,3 +45,34 @@ async def websocket_endpoint(websocket: WebSocket, task_id: str):
         await websocket.send_text(f"Task {task_id} succeeded: {result.result}")
     else:
         await websocket.send_text(f"Task {task_id} failed: {result.result}")
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.username}
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
