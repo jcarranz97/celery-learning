275b38027ec5bd2a08bc571cb5a570934ad586f4
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-02-25 00:39:59 -0600

    Add api call to get test result

--- group1/router.py
+++ group1/router.py
@@ -9,3 +9,11 @@ router = APIRouter()
 async def send_add_task(a: int, b: int) -> TaskId:
     task = add.delay(a, b)
     return TaskId(task_id=task.id)
+
+
+@router.get("/get-result")
+async def get_task_result(task_id: str):
+    task = add.AsyncResult(task_id)
+    if not task.ready():
+        return {"status": "PENDING"}
+    return {"status": "SUCCESS", "result": task.get()}
