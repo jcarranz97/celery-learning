2fd297508492e79a17964c64973cae4d960d48b3
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-02-25 14:05:35 -0600

    Put users database as a dictionaryw

--- auth.py
+++ auth.py
@@ -11,22 +11,39 @@ from fastapi import status
 
 security = HTTPBasic()
 
+users_fake_db = {
+    "stanley": {
+        "username": "stanley",
+        "password": "swordfish",
+        "email": "stanley@gmail.com",
+    },
+    "jcarranz": {
+        "username": "jcarranz",
+        "password": "password",
+        "email": "Juan@gmail.com",
+    },
+}
+
 
 def get_current_username(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)]
 ):
     """Function to authenticate and get the current username."""
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = b"stanleyjobson"
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
+    # This is a very basic implementation of authentication and the users
+    # database is just a fake in-memory dictionary.
+    if credentials.username not in users_fake_db:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = b"swordfish"
+    correct_password_bytes = \
+        users_fake_db[credentials.username]["password"].encode("utf8")
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
-    if not (is_correct_username and is_correct_password):
+    if not is_correct_password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
